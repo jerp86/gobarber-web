ac8f10ed90d75dfd8ae9903468c214f48347feab
Jerp86 <jerp4@hotmail.com>
2020-04-23 16:25:14 -0300

    Rotas autenticadas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Container>Dashboard</Container>;
+};
+
+export default Dashboard;
